51a3e48eeb9cd8074c9b6c03bf44e8e6a1020152
kanishkasrinivasan32 <152157573+kanishkasrinivasan32@users.noreply.github.com>
2025-01-25 22:06:18 +0530

    Add files via upload

---
+++ sumofthreeno.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Sumofthreeno{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b= sc. nextInt();
+int c = sc.nextInt();
+int sum = a+b+c;
+System.out.println("Value of a: "+a);
+System.out.println("Value of b: "+b);
+System.out.println("Value of c: "+c);
+System.out.println("Sum: "+sum);
+}
+}
